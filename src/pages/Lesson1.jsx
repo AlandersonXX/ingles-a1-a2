4b54164434f1935ed8caceac6294674f47c1b633
AlandersonXX <alanderson.s.a@hotmail.com>
2025-06-17 00:13:06 -0300

    Adiciona navegação e estrutura básica do app

--- src/pages/Lesson1.jsx
+++ src/pages/Lesson1.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import lessons from "../data/lessons";
 
 export default function Lesson1() {
+  const lesson = lessons.find(l => l.id === 1);
+
   return (
-    <div>
-      <h2>Lição 1 - Básico</h2>
-      {/* Seu conteúdo da lição */}
-      <p>Conteúdo da primeira lição aqui...</p>
+    <div style={{ padding: "1rem" }}>
+      <h2>{lesson.title}</h2>
+      <div dangerouslySetInnerHTML={{ __html: lesson.content }} />
     </div>
   );
 }
